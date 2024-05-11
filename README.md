1c7692932fcaf376471ca434bbfd77f7c97ca32d
apons <apons@easypromosapp.com>
2024-05-11 16:26:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Photo backup
+
+## Abstract
+
+Download all the photos in google photos account for a given year
+
+The script loops through all the media files in the user's library and downloads
+each file. A folder is created for every day that contains media files.
+
+## Usage
+
+### Parameters
+
+- year: in format yyyy (by default select the current year)
+- folder: The folder where to store files, by default is "photos" but you can specify another folder or unit.
+
+### Examples
+
+- Download all files of year 2024 to photos folder
+
+```sh
+python3 backup.py 2024
+```
+
+- Download all files of year 2003 to photos_backup folder
+
+```sh
+python3 backup.py 2003 photos_backup
+```
+
+- Download all the current year files to photos folder
+
+```sh
+python3 backup.py
+```
+
+- Using linux, shell to launch the current year backup
+
+```sh
+./backup.sh
+```
+
+## Authentication step by step guide
+
+- Make sure you can connect with your google account to developers.google.com
+- Go to <https://console.cloud.google.com/apis/library/photoslibrary.googleapis.com> and Enable API
+- ![Enable API image](images/enable-api.png "Enable API image")
+- Go to <https://console.cloud.google.com/apis/library>
+- Click "Create Credentials"
+- ![Create credentials image](images/create-credentials.png "Create credentials image")
+- Select "User Data"
+- ![User data image](images/user-data.png "User data image")
+- Add, at least photoslibrary.readonly scope to the credentials
+- ![Credentials scope image](images/scope.png "Credentials scope image")
+- You'll see the scope added to the "Sensitive scopes" list in the credentials configuration page
+- ![Sensitive scopes image](images/sensitive-scopes.png "Sensitive scopes image")
+- Select "Desktop app" in the type of application
+- ![Select desktop app image](images/select-desktop-app.png "Select desktop app image")
+- And give a name to the client application
+- ![OAuth Client ID image](images/openid-client.png "OAuth Client ID image")
+- Download yor credentials file, you will get a client_secret_xxxxxyyyy.json
+- Rename your credentials file to only client_secret.json and store it in this folder (the project root folder)
+- Go to the Oauth Consent Screen and add your email in the section "Test users" if you add your user you don't need to publish the app for everyone
+- Launch ./backup.sh or python3 backup.py
+- You'll see an authorization url, open it in a browser, select your user (the one that you've selected as "Test user" before), grant acces to the app as a tester and finally close this browser
+- If everything has worked out you'll see messages that the files are already downloading to your photos folder.
+- A token.pickle file has been created so the authorization is cached for some days, if the token.pickle token expires you'll need to grant permissions again.
+
+## Guides
+
+- <https://developers.google.com/photos/library/guides/overview>
---
+++ backup.py
@@ -0,0 +1,163 @@
+from __future__ import print_function
+import os, pickle, json, requests, imageio, sys, re, datetime
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.http import MediaIoBaseDownload
+
+# Setup the Photo v1 API
+SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
+creds = None
+
+# Check if running on local environment
+#is_local_env = os.getenv('IS_LOCAL') == 'true'
+
+if os.path.exists("token.pickle"):
+    with open("token.pickle", "rb") as tokenFile:
+        print("Loading pickle file")
+        creds = pickle.load(tokenFile)
+else:
+    flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
+    creds = flow.run_local_server(port=0)
+    with open("token.pickle", "wb") as tokenFile:
+        pickle.dump(creds, tokenFile)
+
+
+if not creds:
+    print("No creds found, remove token.pickle file and try again")
+    sys.exit(1)
+    
+# Build the Photos v1 API service
+service = build('photoslibrary', 'v1', credentials=creds, static_discovery=False)
+
+def download_all_media(directory, year):
+    print(f"Backing up all the media files of year {year} to the folder {directory}")
+    # Create the download directory if it doesn't exist
+    os.makedirs(directory, exist_ok=True)
+
+    try:
+        pageToken=''
+        results = service.mediaItems().search(body={'pageSize':100, 'orderBy':'MediaMetadata.creation_time desc', 'filters': {'dateFilter':{'dates': [{'year': year}]}}, 'pageToken':pageToken}).execute()
+        pageToken=results.get('nextPageToken')
+        media_items = results.get('mediaItems', [])
+    except Exception as e:
+        print(f"Error retrieving media items: {e}")
+        return
+
+    skipped=0
+    while media_items is not None:
+        # Download each media item
+        for media_item in media_items:
+            if (skipped>=5):
+                print(f"Skipped {skipped} media items, finishing!")
+                return
+            
+            day = media_item['mediaMetadata']['creationTime'][:10]
+            folder = directory+'/'+day
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+                print("Folder "+folder+" created.")
+
+            media_item_filename = scapeFilename(media_item['filename'])
+            filename = os.path.join(directory, day, media_item_filename+'.json')
+            request = service.mediaItems().get(mediaItemId=media_item['id'])
+
+            # Download the media item
+            try:
+                with open(filename, 'wb') as fh:
+                    downloader = MediaIoBaseDownload(fh, request)
+                    done = False
+                    while not done:
+                        status, done = downloader.next_chunk()
+                        #print(f'Downloading {filename}: {int(status.progress() * 100)}%')
+            except Exception as e:
+                print(f"Error downloading {filename}: {e}")
+        
+            with open(filename) as f:
+                data = f.read()
+                parsed_data = json.loads(data)
+                #print(parsed_data)
+                url=parsed_data['baseUrl']
+                media_filename = filename.rstrip(".json")
+                width=parsed_data['mediaMetadata']['width']
+                height=parsed_data['mediaMetadata']['height']
+                url=url+sufixByType(media_filename, width, height)
+                numDownloads=downloadFile(url, media_filename)
+                if (numDownloads==0):
+                    skipped=skipped+1
+        try:
+            if pageToken is None:
+                print('No more pages to scan, exiting!')
+                return
+
+            results = service.mediaItems().search(body={'pageSize':100, 'orderBy':'MediaMetadata.creation_time desc', 'filters': {'dateFilter':{'dates': [{'year': year}]}}, 'pageToken':pageToken}).execute()
+            pageToken=results.get('nextPageToken')
+            media_items = results.get('mediaItems', [])
+        except Exception as e:
+            print(f"Error retrieving media items: {e}")
+            return
+
+def scapeFilename(filename):
+    specials = r'[/\\?%*:|"<>]'
+    scaped = re.sub(specials, '_', filename)
+    return scaped
+
+def sufixByType(file_name, width, height):
+    file_extension = file_name.lower().split('.')[-1]
+    images = ['bmp', 'jpg', 'jpeg', 'gif', 'png', 'heic', 'tiff', 'ico', 'webp', 'pdf']
+    videos = ['3gp', 'mmv', '3g2', 'mod', 'asf', 'mov', 'avi', 'mp4', 'divx', 'mpg', 'm2t', 'mts', 'm2ts', 'tod', 'm4v', 'wmv']
+
+    if file_extension in images:
+        return '=w'+width+'-h'+height
+    elif file_extension in videos:
+        return '=dv'
+    else:
+        raise ValueError(f"Unknown file type in file {file_name}")
+
+
+def downloadFile(url, destination):
+    if os.path.exists(destination):
+        print(f'The file {destination} already exists, skipping.')
+        return 0
+    #print(url)
+    print(f'Downloading {destination} media file')
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(destination, 'wb') as f:
+            f.write(response.content)
+        convertHeic2JpgIfNeeded(destination)
+        return 1
+    else:
+        print("Error downloading file.")
+        return 0
+
+def convertHeic2JpgIfNeeded(filename):
+    if filename.endswith('.HEIC'):
+        jpg_filename = filename[:-5] + '.jpg'
+        print(f"Transforming file .heic {filename} to {jpg_filename}")
+        try:
+            image = imageio.v2.imread(filename)
+            imageio.imwrite(jpg_filename, image)
+            print(f"DONE! file {filename} converted to {jpg_filename}")
+        except Exception as e:
+            print(f"Error converting {filename} to JPG: {e}")
+
+def main(year=None, download_directory='photos'):
+    if year is None:
+        year = datetime.datetime.now().year
+        print(f"Automatically selectiong current year ({year})")
+    download_all_media(download_directory, year)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        year = int(sys.argv[1])  # year is the first argument
+        if len(sys.argv) > 2:
+            if (sys.argv[2]!=None):
+                folder=sys.argv[2]
+                main(year, folder)
+                sys.exit(0)
+
+        main(year)
+    else:
+        print("Please, enter a year as first argument.")
+        main()
